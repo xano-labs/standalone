c1aaa1afeac2769c5d1b41253ce0019e3ba4a1b8
Sean Montgomery <sean@xano.com>
2024-03-07 11:07:19 -0800

    Remove old file

--- env.sh
+++
@@ -1,7 +0,0 @@
-# view README.md for more details
-# https://gitlab.com/xano/standalone/
-
-export XANO_DOMAIN=app.xano.com
-export XANO_PORT=4200
-export XANO_INSTANCE=CHANGE_ME
-export XANO_TOKEN=CHANGE_ME
